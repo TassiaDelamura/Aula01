3bd779426191968624c2f902d0d2323bc69d6009
Tassia Delamura <tassia.delamura@hotmail.com>
2021-06-10 19:57:10 -0300

    Criadas as funcionalidades

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -6,7 +6,7 @@ public class Calculadora {
 		Scanner leitor = new Scanner (System.in);
 		double valor1;
 		double valor2;
-		double soma;
+		double soma, divisao,multiplicacao,subtracao;
 		System.out.println("Porfavor, digite um valor:");
 		
 		valor1 = leitor.nextDouble();
@@ -14,7 +14,15 @@ public class Calculadora {
 		valor2 = leitor.nextDouble();
 		
 		soma = valor1 + valor2;
-		System.out.println("A soma dos dois valores informados é:");
+		subtracao = valor1 - valor2;
+		multiplicacao = valor1 * valor2;
+		divisao = valor1 / valor2;
+		
+		System.out.println("A soma dos dois valores informados é " + soma);
+		System.out.println("A subtração dos dois valores informados é " + subtracao);
+		System.out.println("A multiplicacao dos dois valores informados é " + multiplicacao);
+		System.out.println("A divisão dos dois valores informados é " + divisao);
+
 		
 		leitor.close();
 		
